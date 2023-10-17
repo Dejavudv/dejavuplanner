f7c66c89dd1f0d6e22e033cb9cae136c8c96f60d
dejavudv <elay82ghanbari@gmail.com>
2023-10-18 01:36:02 +0330

    cart show update delete

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,10 @@ urlpatterns = [
     path("search/", search_view, name="search"),
     path("filter-products/", filter_product, name="filter-product"),
     path("add-to-cart/", add_to_cart, name="add-to-cart"),
+    path("cart/", cart_view, name="cart"),
+    path("delete-from-cart/", delete_item_from_cart, name="delete-from-cart"),
+    path("update-cart/", update_cart, name="update-cart"),
+
 
 
 
